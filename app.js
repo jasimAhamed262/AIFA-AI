67d1d3f2841ceb7464e2245b3fff6c19b9c54a63
jasimAhamed262 <jasimahamed262@gmail.com>
2025-07-03 22:22:06 +0530

    update: added aurax integreated api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+
 // GSAP plugins are already loaded via CDN in the HTML file
 const gsap = window.gsap // Declare gsap variable
 const ScrollTrigger = window.ScrollTrigger // Declare ScrollTrigger variable
-
 
 
 
@@ -357,3 +357,161 @@ const handleFormSubmit = (e) => {
 }
 
 promptForm.addEventListener("submit", handleFormSubmit)
+
+
+
+/** aurax handling */
+const container = document.querySelector(".aurax-container")
+const chatsContainer = document.querySelector(".chats-container")
+const auraxPromptForm = document.querySelector(".aurax-prompt-form")
+const auraxPromptInput = auraxPromptForm.querySelector(".aurax-prompt-input")
+const fileInput = auraxPromptForm.querySelector("#file-input")
+
+
+//api setup
+const API_KEY = "AIzaSyCj8JBoiFF_CFKVq34M52za5G9F1mcZ9ms";
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`
+
+
+
+
+
+
+let userMessage = "";
+const chatHistory =[]
+
+
+//function to create message elements
+const createMsgElement = (content,...classes) => {
+  const div = document.createElement("div")
+  div.classList.add("message", ...classes)
+  div.innerHTML = content
+  return div
+}
+
+//scroll to the bottom of  the container
+const scrollToBottom = () => container.scrollTo({top: container.scrollHeight, behavior: "smooth"})
+
+
+
+//simulate typing effect for bot responses
+const typingEffect = (text, textElement, botMsgDiv) => {
+  textElement.textContent =""
+  const words = text.split(" ")
+  let wordIndex =0;
+
+  //set an interval to type each word
+const typingInterval = setInterval(() => {
+  if(wordIndex < words.length){
+    textElement.textContent += (wordIndex ===0 ? "": " ") + words[wordIndex++];
+    botMsgDiv.classList.remove("loading");
+    scrollToBottom();
+  } else{
+    clearInterval(typingInterval);
+  }
+}, 40)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//make api call and generate bot response
+const generateResponse = async (botMsgDiv) => {
+
+  const textElement = botMsgDiv.querySelector(".message-text")
+
+
+
+  //add user message to the chat history
+  chatHistory.push({
+     role: "user",
+     parts: [{text:userMessage}]
+  })
+
+
+
+  try{
+    //send the cht history to the api to get a response
+      const ressponse = await fetch(API_URL,{
+         method: "POST",
+         headers: {"Content-Type": "application/json"},
+         body: JSON.stringify({contents: chatHistory})
+      });
+
+
+      const data = await ressponse.json()
+      if(!ressponse.ok) throw new Error(data.error.message)
+      
+      //Process the response text and display with typing effect
+      const responseText = data.candidates[0].content.parts[0].text.replace(/\*\*([^*]+)\*\*/g, "$1").trim()
+      typingEffect(responseText, textElement, botMsgDiv )
+      chatHistory.push({role:"model",parts:[{text:responseText}]})
+
+  } catch (error){
+        console.log(error)
+  }
+}
+
+
+
+
+//handleform submission
+const handleeFormSubmit = (e) => {
+  e.preventDefault()
+  userMessage = auraxPromptInput.value.trim()
+  if(!userMessage) return
+
+
+  auraxPromptInput.value =""
+
+
+  //user message html and add in the container
+  const userMsgHTML = `<p class="message-text"></p>`
+  const userMsgDiv = createMsgElement(userMsgHTML,"user-message")
+
+  userMsgDiv.querySelector(".message-text").textContent = userMessage;
+  chatsContainer.appendChild(userMsgDiv)
+  scrollToBottom()
+
+  
+  setTimeout(() => {
+
+     //Generate bot message Html and add in the chats container in 600ms
+  const botMsgHTML = `<img src="aurax.svg" class="avatar">
+<p class="message-text">Just a sec...</p>`
+  const botMsgDiv = createMsgElement(botMsgHTML,"bot-message","loading")
+  chatsContainer.appendChild(botMsgDiv)
+  scrollToBottom()
+  generateResponse(botMsgDiv)
+
+  }, 600)
+}
+
+//handle file input change
+fileInput.addEventListener("change", () => {
+  const file = fileInput.files[0]
+  if(!file) return
+
+  console.log(file)
+})
+
+
+
+
+
+
+auraxPromptForm.addEventListener("submit",handleeFormSubmit)
+auraxPromptForm.querySelector("#add-file-btn").addEventListener("click", () => fileInput.click())
+
+
